9633b7de86ad7b0134ce7e713b2ea96b434df8f1
Arnoud de Boer <a.deboer@geonovum.nl>
2023-11-22 09:40:29 +0100

    GMW 1.0 in respec?

---
+++ gmw-1.0/config.js
@@ -0,0 +1,26 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  //canonicalURI: "WV",
+ // publishDate: "2016-10-18",
+  editors: [
+    {
+      name: "TNO Geologische Dienst Nederland",
+      url: "https://www.tno.nl/nl/",
+    },
+  ],
+  shortName: "bro-gmw",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/GMW",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
