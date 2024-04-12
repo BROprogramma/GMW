96b0967c1105809162845df79a048b2063c9eeed
Thijs Knapen <thijs.knapen@tno.nl>
2024-04-12 11:00:40 +0200

    ./js/config.js weer teruggezet naar instellingen voor werkversie.

--- js/config.js
+++ js/config.js
@@ -3,11 +3,11 @@ let respecConfig = {
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Basisregistratie Ondergrond Catalogus Grondwatermonitoringput",
-  subtitle: "Versie 1.099",
+  subtitle: "Versie 1.09",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
+  specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
-  specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
@@ -30,7 +30,7 @@ let respecConfig = {
   shortName: "gmw",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2024-04-12",
+  //publishDate: "2024-04-12",
   //custom header lines  
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
